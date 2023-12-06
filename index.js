0f5c89d609cbb6aee3ab675a19f5f4e8a29eb5b9
AthaRazin <atharazin18@gmail.com>
2023-12-06 20:41:02 +0700

    API

--- index.js
+++ index.js
@@ -18,6 +18,31 @@ app.get("/rooms", async (req, res) => {
   res.status(200).send(room);
 });
 
+app.get("/reservations", async (req, res) => {
+  const reservations = await prisma.reservations.findMany();
+  res.status(200).send(reservations);
+});
+
+app.post("/rooms", async (req, res) => {
+  const { room } = req.body;
+  if (!room) res.status(400).json({ message: "Name is required" });
+  const newRoom = await prisma.room.create({
+    data: {
+      name: room,
+      room_number: 1,
+      room_type: 1,
+      number_of_guests: 1,
+      harga: 500000,
+      room_desc: 1,
+      img: 1,
+    },
+  });
+  res.status(201).json({
+    message: "Room created",
+    data: newRoom,
+  });
+});
+
 app.all("*", async (req, res) => {
   res.json({
     message: "Routes you're looking is not found",

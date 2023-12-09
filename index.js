9693793041a762cc47cb4f99f91575f20180721a
Fitriani Halimatus Sadiyyah <148773301+fitrianihs@users.noreply.github.com>
2023-12-09 22:54:02 +0700

    fix eror
    coba ngatasin eror dengan ubah port

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is already running at ${PORT}`);
 });
